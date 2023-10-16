fa1d499cc4dca7010e3b0d7804a4908a454d25f0
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-10-16 03:08:04 +0300

    Header socials

--- my_prortfolio/src/Components/Header/Header.tsx
+++ my_prortfolio/src/Components/Header/Header.tsx
@@ -40,9 +40,9 @@ const Header: React.FC<Iprops> = ({ linked }: Iprops) => {
         </nav>
 
         <div className="social-links">
-          <a href="#" className="twitter"><FaTwitter /></a>
-          <a href="#" className="facebook"><FaFacebookF /></a>
-          <a href="#" className="instagram"><FaInstagram /></a>
+          <a href="https://twitter.com/danmogaka" className="twitter"><FaTwitter /></a>
+          <a href="https://www.facebook.com/dhmogaka/" className="facebook"><FaFacebookF /></a>
+          <a href="https://www.instagram.com/excursion_ke/" className="instagram"><FaInstagram /></a>
         </div>
       </div>
     </header>
