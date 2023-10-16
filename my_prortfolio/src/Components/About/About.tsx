87dfb1e2c3e60710469219ed191688eb83958e82
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-10-16 03:24:18 +0300

    Update About.tsx

--- my_prortfolio/src/Components/About/About.tsx
+++ my_prortfolio/src/Components/About/About.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import Header from '../Header/Header'
+import { BiChevronRight } from 'react-icons/bi'
 
 const About = () => {
   return (
     <>
-      <Header linked={true}/>
-      
-      
+      <Header linked={true}/>  
   <section id="about" className="about section-show">
     <div className="about-me container">
-
       <div className="section-title">
         <h2>About</h2>
         <p>Learn more about me</p>
@@ -20,41 +18,39 @@ const About = () => {
           <img src="assets/img/me.jpg" className="img-fluid" alt=""/>
         </div>
         <div className="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
-          <h3>UI/UX &amp; Graphic Designer</h3>
+          <h3>Film Maker &amp; Excursionist</h3>
           <p className="fst-italic">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.
+            I am a dedicated excursionist, capturing the world's wonders through film and exploration,
+            with a special passion for the enchanting landscapes of Kenya. 🌍📷🌄🇰🇪
           </p>
           <div className="row">
             <div className="col-lg-6">
               <ul>
-                <li><i className="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
-                <li><i className="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
-                <li><i className="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
-                <li><i className="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                <li><BiChevronRight/></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
+                <li><BiChevronRight/> <strong>Website:</strong> <span>www.example.com</span></li>
+                <li><BiChevronRight/> <strong>Phone:</strong> <span>+254 720 899 837</span></li>
+                <li><BiChevronRight/></i> <strong>City:</strong> <span>Nairobi, Kenya</span></li>
               </ul>
             </div>
             <div className="col-lg-6">
               <ul>
-                <li><i className="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
-                <li><i className="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
-                <li><i className="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
-                <li><i className="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                <li><BiChevronRight/> <strong>Age:</strong> <span>30</span></li>
+                <li><BiChevronRight/> <strong>Degree:</strong> <span>Master</span></li>
+                <li><BiChevronRight/> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
+                <li><BiChevronRight/> <strong>Freelance:</strong> <span>Available</span></li>
               </ul>
             </div>
           </div>
           <p>
-            Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
-            Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
+            I'm a passionate excursionist and filmmaker, deeply inspired by the captivating landscapes of Kenya 🌍📷🌄🇰🇪. With a fervent desire to explore the world,
+            I invite you to join me on my global adventures, as we embark on a shared journey through the lens of my camera, capturing the beauty of our planet.
           </p>
         </div>
       </div>
 
     </div>
     <div className="counts container">
-
       <div className="row">
-
         <div className="col-lg-3 col-md-6">
           <div className="count-box">
             <i className="bi bi-emoji-smile"></i>
